e8177ad9f5fe82a8e8e56304f99096c36e44645e
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-08 10:09:02 +0530

    added transaction minimizer code

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask,request
+from transaction_minimizer import cash_flow_minimizer
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def home():
     return "hello"
 
+@app.route('/min-flow', methods=['POST'])
+def getminFlow():
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        transactions = []
+        # ['A', 'B', 5]
+
+        for transaction in data['transactions']:
+            curr_trans = []
+            curr_trans.append(transaction['payer'])
+            curr_trans.append(transaction['payee'])
+            curr_trans.append(transaction['amount'])
+            transactions.append(curr_trans)
+
+        print(transactions)
+        result = cash_flow_minimizer(transactions)
+
+
+    return {'result' : result}
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
